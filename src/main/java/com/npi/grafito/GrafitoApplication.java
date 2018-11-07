a1f17274675027c4d2595fd700347f11a43c4059
npirard <npirard.com@gmail.com>
2018-11-07 22:08:48 +0100

    Init project

---
+++ src/main/java/com/npi/grafito/GrafitoApplication.java
@@ -0,0 +1,12 @@
+package com.npi.grafito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrafitoApplication.class, args);
+	}
+}
