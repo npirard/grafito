dfdd9c4336a3d7aa8c3b370982431c5afc599620
npirard <npirard.com@gmail.com>
2018-11-08 21:54:23 +0100

    Call the back from the front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount()  {
+    setInterval(this.hello, 250);
+  }
+
+  hello = () => {
+    fetch('/api/hello')
+    .then(response => response.text())
+    .then(message => {
+      this.setState({message: message});
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        Welcome to Grafito !
+        Welcome to Grafito ! {this.state.message}
       </div>
     );
   }
